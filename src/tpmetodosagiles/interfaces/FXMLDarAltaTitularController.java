c22eafe0a10c36d1de294b88222a88669df4f1ed
es279 <emirsonzogni279@gmail.com>
2019-11-03 02:33:32 -0300

    Validación interface DarDeAltaTitular

--- src/tpmetodosagiles/interfaces/FXMLDarAltaTitularController.java
+++ src/tpmetodosagiles/interfaces/FXMLDarAltaTitularController.java
@@ -7,18 +7,26 @@ package tpmetodosagiles.interfaces;
 
 import java.io.File;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.temporal.TemporalUnit;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
 import tpmetodosagiles.entidades.Usuario;
 import tpmetodosagiles.gestores.GestorDeDatosDeInterface;
+import tpmetodosagiles.layouts.TextFieldSoloLetras;
+import tpmetodosagiles.layouts.TextFieldSoloNumeros;
 
 /**
  * FXML Controller class
@@ -28,17 +36,17 @@ import tpmetodosagiles.gestores.GestorDeDatosDeInterface;
 public class FXMLDarAltaTitularController implements Initializable {
 
     @FXML
-    private TextField tfNumeroDocumento;
+    private TextFieldSoloNumeros tfNumeroDocumento;
     @FXML
-    private TextField tfNombreTitular;
+    private TextFieldSoloLetras tfNombreTitular;
     @FXML
-    private TextField tfApellidoTitular;
+    private TextFieldSoloLetras tfApellidoTitular;
     @FXML
-    private TextField tfNroAltura;
+    private TextFieldSoloNumeros tfNroAltura;
     @FXML
-    private TextField tfNroInterno;
+    private TextFieldSoloNumeros tfNroInterno;
     @FXML
-    private TextField tfPiso;
+    private TextFieldSoloNumeros tfPiso;
     @FXML
     private ComboBox cbTipoDocumento;
     @FXML
@@ -53,6 +61,8 @@ public class FXMLDarAltaTitularController implements Initializable {
     private ComboBox cbObservaciones;
     @FXML
     private ComboBox cbClaseLicencia;
+    @FXML
+    private DatePicker dpFechaNacimiento;
     @FXML
     private ImageView imgFotoTitular;
     
@@ -94,6 +104,84 @@ public class FXMLDarAltaTitularController implements Initializable {
         
         ObservableList <String> claseLicencia = FXCollections.observableArrayList( GestorDeDatosDeInterface.getLicenciasBasicas() );
         cbClaseLicencia.setItems(claseLicencia);
+        
+        tfNumeroDocumento.setLongitudMaxima(8);
+        tfNroAltura.setLongitudMaxima(6);
+        tfNroInterno.setLongitudMaxima(5);
+        tfPiso.setLongitudMaxima(2);
     }
     
+    @FXML
+    private void darDeAltaTitular(ActionEvent event){
+        if (this.datosValidos())
+            return;
+    }
+    
+    private boolean datosValidos(){
+        boolean datosCorrectos = true;
+        StringBuffer errores = new StringBuffer("");
+        
+        if (cbTipoDocumento.getValue() == null){
+            errores.append("-Debe seleccionar el tipo de documento de identificación nacional del titular.\n");
+            datosCorrectos = false;
+        }
+        if (tfNumeroDocumento.getText().length() < 7){
+            errores.append("-El campo 'Nro de Documento' debe contener al menos 7 caracteres.\n");
+            datosCorrectos = false;
+        }
+        if (tfNombreTitular.getText().isEmpty()){
+            errores.append("-El campo 'Nombres' no puede estár vacío.\n");
+            datosCorrectos = false;
+        }
+        if (tfApellidoTitular.getText().isEmpty()){
+            errores.append("-El campo 'Apellidos' no puede estár vacío.\n");
+            datosCorrectos = false;
+        }
+        if (dpFechaNacimiento.getValue() == null){
+            errores.append("-El campo 'Fecha de Nacimiento' no puede estár vacío.\n");
+            datosCorrectos = false;
+        }
+        else if (dpFechaNacimiento.getValue().compareTo(LocalDate.now().minusYears(17)) > 0){    //Si el titular tiene menos de 17 años
+            errores.append("-El titular debe tener al menos 17 años.\n");
+            datosCorrectos = false;
+        }
+        if (cbCalleTitular.getValue() == null){
+            errores.append("-El campo 'Calle' no puede estár vacío.\n");
+            datosCorrectos = false;
+        }
+        if (tfNroAltura.getText().isEmpty()){
+            errores.append("-El campo 'Altura' no puede estár vacío.\n");
+            datosCorrectos = false;
+        }
+        if (cbGrupoSanguinio.getValue() == null){
+            errores.append("-Debe seleccionar una de las opciones de 'Grupo Sanguínio'.\n");
+            datosCorrectos = false;
+        }
+        if (cbEsDonante.getValue() == null){
+            errores.append("-Debe especificar si el titular es donante o no.\n");
+            datosCorrectos = false;
+        }
+        if (cbSexo.getValue() == null){
+            errores.append("-Debe seleccionar el sexo de nacimiento del titular.\n");
+            datosCorrectos = false;
+        }
+        if (cbClaseLicencia.getValue() == null){
+            errores.append("-Debe seleccionar una de las opciones de 'Clase de Licencia Solicitada'.\n");
+            datosCorrectos = false;
+        }
+        //TODO: Emir-Luciano: verificar que se haya seleccionado una fotografía (que no sea la de 'por defecto')
+        
+        if (!datosCorrectos){
+            Alert mensajeErrores = new Alert(Alert.AlertType.INFORMATION);
+            mensajeErrores.setTitle("Datos faltantes/incorrectos");
+            mensajeErrores.setHeaderText("Debe corregir los siguientes puntos antes de proseguir");
+            mensajeErrores.setContentText(errores.toString());
+            
+            mensajeErrores.initModality(Modality.WINDOW_MODAL);
+            mensajeErrores.show();
+        }
+        //System.out.println(errores);
+        
+        return datosCorrectos;
+    } 
 }
---
+++ src/tpmetodosagiles/layouts/TextFieldSoloNumeros.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpmetodosagiles.layouts;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author usuario
+ */
+public class TextFieldSoloNumeros extends TextField {
+    private int longitudMaxima = 10;
+    
+    
+    @Override
+    public void replaceText(int start, int end, String text)
+    {
+        if (validar(text))
+        {
+            super.replaceText(start, end, text);
+        }
+    }
+
+    @Override
+    public void replaceSelection(String text)
+    {
+        if (validar(text))
+        {
+            super.replaceSelection(text);
+        }
+    }
+
+    private boolean validar(String text)
+    {
+        //this.getText() retorna el último texto valido del TextField (sin el último valor ingresado, que está en la variable 'text')
+        return (text.matches("[0-9]*") && this.getText().length() < this.longitudMaxima);
+    }
+    
+    public void setLongitudMaxima(int longitud){
+        this.longitudMaxima = longitud;
+    }
+}
